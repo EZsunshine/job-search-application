41db41240981f77b7aebfa5faa22cb4800fe634c
Elaine Zhu <ezsun@MacBook-Pro.local>
2023-01-15 22:50:04 -0500

    added signin and register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
+import React, { useState } from "react";
 import Navigation from './Navigation.js'
+import  Login  from "./Login";
+import Register from "./Register";
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
   return (
     <div>
+      <div>
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+      }
+    </div>
       <Navigation />
     </div>
   );
